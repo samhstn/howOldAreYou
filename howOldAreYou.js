4bb9423405f5f63aae23d2bb3bddf6f19cc8fcd9
Andrew MacMurray <a.macmurray@icloud.com>
2015-12-07 16:31:19 +0000

    added some error handling to js file

--- howOldAreYou.js
+++ howOldAreYou.js
@@ -1,28 +1,39 @@
 function readBox() {
-	// change for the 3 different input boxes (instead of one)
-	var day = parseInt(document.getElementById('day').value);
-	var month = parseInt(document.getElementById('month').value);
-	var year = parseInt(document.getElementById('year').value);
+	// changed for the 3 different input boxes (instead of one)
+	// used these so if no dates in boxes the 'not correct date' message appears
+	var dayVal = document.getElementById('day').value;
+	var monthVal = document.getElementById('month').value;
+	var yearVal = document.getElementById('year').value;
 
-	// maybe use object literals (instead of new Object()) a bit more readable
+	var day = parseInt(dayVal);
+	var month = parseInt(monthVal);
+	var year = parseInt(yearVal);
+
+	// used new Date to get current time
 	var now = new Date();
-	// console.log(now);
+
 	var today = {
 		day: now.getDay(),
 		month: now.getMonth(),
-		year: now.getYear()
+		// apparently getYear() is being dropped from JS because of the 'year 2000 problem' getFullYear() is its replacement
+		year: now.getFullYear()
 	}
-	// console.log(getDay());
 
+	// maybe use object literals (instead of new Object()) a bit more readable
 	var born = {
 		day: day,
 		month: month,
 		year: year
 	}
 
-	if (day > 31 || month > 12) {
-		document.getElementById("demo").innerHTML = "That is not a correct date!";
-	} else {
+	function invalidDate() {
+		document.getElementById("demo").innerHTML = "That is not a correct date! Try again";
+	}
+
+	// first line checks for no input
+	if (dayVal === '' || monthVal === '' || yearVal === '') invalidDate()
+	else if (day > 31 || month > 12) invalidDate()
+	else {
 		if (today.day >= born.day && today.month >= born.month) {
 			var days = today.day - born.day;
 			var months = today.month - born.month;
